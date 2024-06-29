868d12a5b13749a2b908a7ed1febd720b783e9ef
Mark Peters <mpskeeter@gmail.com>
2024-06-29 12:01:20 -0400

    chore: create BaseCrudService

--- src/app/core/services/base-crud.service.ts
+++ src/app/core/services/base-crud.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@angular/core';
-import { BaseEntity } from '../interfaces/base-entity'
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-
+import { AuthService } from './auth.service';
+import { BaseEntity } from '../interfaces/base-entity'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseCrudService<T extends BaseEntity> {
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  baseURL: string = 'https://api.github.com';
+  endpointType: string = '';
 
   items = new BehaviorSubject<T[]>([]);
   items$ = this.items.asObservable();
 
-  setValue = (newValue:T[]) => this.items.next(newValue);
-
   constructor() { }
+
+  getAll = () => {
+    this.http.get<T[]>(`${this.baseURL}${this.endpointType}`).subscribe(items => this.items.next(items));
+  }
+
+  getAllForRepository = (repository: string = '') => {
+    if (repository !== '') {
+      this.http.get<T[]>(`${this.baseURL}/repos/${this.authService.owner}/${repository}/${this.endpointType}`)
+        .subscribe((items) => this.items.next(
+          items.map((item) => 
+            item.name
+            ? item
+            : {
+              ...item,
+              name: item.sha,
+            }
+          )
+        ));
+    }
+  } 
+
 }
