2bda4707b714190766551a0584583ef7bf948689
Ethan <2002ethankuehl@gmail.com>
2024-07-26 14:12:27 -0400

    chore(ecr-form):built capabilities detail

--- src/app/ECR/components/ecr-page-impact-capabilities-detail/ecr-page-impact-capabilities-detail.component.ts
+++ src/app/ECR/components/ecr-page-impact-capabilities-detail/ecr-page-impact-capabilities-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, effect, inject, input } from '@angular/core';
+import { Component, computed, effect, inject, input } from '@angular/core';
 import { CommitService } from '../../../github';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-ecr-page-impact-capabilities-detail',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
+  providers:[CommitService],
   templateUrl: './ecr-page-impact-capabilities-detail.component.html',
   styleUrl: './ecr-page-impact-capabilities-detail.component.scss'
 })
@@ -12,6 +14,15 @@ export class EcrPageImpactCapabilitiesDetailComponent {
   itemNumber=input<number>();
   sha=input<string>('');
   service=inject(CommitService);
+  module=computed(() => {
+    const message= this.service.item()?.commit.message;
+    console.log(message);
+    const firstChar= (message?.indexOf( "(") || 0) +1; 
+    const lastChar= message?.indexOf( ")" ) || message?.length; 
+    return message?.substring(firstChar, lastChar);
+
+});
+
 
   eff = effect(() => this.service.getById(this.sha()))
 }
